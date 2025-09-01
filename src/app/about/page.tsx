70e986c5785f67b394ab633cff6758bfa7b87f9a
wenziwei <maweidaweedkiller@gmail.com>
2025-09-01 17:10:21 +0800

    修改後的軟體快照

---
+++ src/app/about/page.tsx
@@ -0,0 +1,6 @@
+function AboutPage(){
+    console.log("hey from about page");
+    return <div>Hello from AboutPage</div>;
+}
+
+export default AboutPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+ "use client";
+
+ import { Button } from '@/components/ui/button';
+ 
+ 
+ import {SignInButton,SignedIn,SignedOut,UserButton} from '@clerk/nextjs';
+ 
+ 
+ export default function Home(){
+  return(
+    <div className='m-4' >
+         <h1>home page content</h1>
+
+              
+
+    </div>
+  )
+}
